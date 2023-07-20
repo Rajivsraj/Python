4a6e0d81b22c5c58d1d4840d2d4c8da813245578
Rajiv <rajivsraj78@gmail.com>
2023-07-20 10:53:09 +0530

    tuple

---
+++ demo14_tuple.py
@@ -0,0 +1,59 @@
+"""tpl = (10, 20, 30, 50)
+
+# print(tpl)
+lst = list(tpl)
+lst[0] = 200
+
+print(lst)
+
+tpl = tuple(lst)
+
+print(type(tpl))"""
+
+"""
+tpl1 = (10, 20, 30)
+tpl2 = (1, 2, 3)
+
+tpl1+=tpl2
+print((tpl1))
+# print(tpl1+tpl2)"""
+
+"""
+tpl = (30, 430, 45)
+del tpl
+print(tpl)"""
+
+
+"""
+tpl = (30, 430, 45) * 2
+lst = [34,34,34] * 2
+str = "hello" * 2
+print(tpl)
+print(lst)
+print(str)"""
+
+"""
+lst = []
+n = input("Enter how a no: ")
+for x in range(int(n)):
+    lst.append(int(input("Enter element")))
+tpl = tuple(lst)
+print(tpl)"""
+
+"""
+lst = [("name", "rahul"),("age", 120)]
+dic = dict(lst)
+print(dic)"""
+
+"""
+lst = (3.,45,45,4,5,4,6,67,5,)
+
+x = lst.count(5)
+print(x)
+"""
+
+"""
+lst = (3.,45,45,4,5,4,6,67,5,)
+x = lst.index(45)   # return the index only
+print(x)
+"""
