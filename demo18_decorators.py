1b1b3df18350334e136837eead361d7eb256b49e
Rajiv <rajivsraj78@gmail.com>
2023-08-08 10:45:38 +0530

    Map, reduce

--- demo18_decorators.py
+++ demo18_decorators.py
@@ -1,5 +1,6 @@
 # decorator is a function that accept a function as an argument
 # decorator is used to modfiy existing functionality of a function with modify the original method
+"""
 def dec(fun):
     def inner():
         fun()
@@ -24,4 +25,48 @@ def total():
     return 10+20
 
 
-print(total())
+print(total())"""
+
+"""
+def dec(fun):
+    print(fun)
+    def inner():
+        print("Hi")
+        fun()
+    return inner
+
+@dec
+def show():
+    print("Hello")
+
+
+# x = dec(show)
+# x()
+show()
+"""
+
+lst = [45, 46, 72, 734,  66, 64]
+def mul(no):
+    return no*2
+
+#
+# x = map(mul, lst)
+# print(x)
+#
+# for a in x:
+#     print(a)
+
+
+from functools import *
+lst = [x for x in range(1,6)]
+lst = [34,345,34,6,54,6,45]
+print(lst)
+def abc(a, b):
+    return a*b
+
+# x = reduce(abc, lst)
+x = reduce(lambda a,b: a*b, lst)
+print(x)
+
+# reduce
+# factorial
