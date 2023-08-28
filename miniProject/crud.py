b0de0145c3606ec798131045860154617b5eb1c9
Rajiv <rajivsraj78@gmail.com>
2023-08-28 13:21:55 +0530

    update mini project

--- miniProject/crud.py
+++ miniProject/crud.py
@@ -1,4 +1,6 @@
+
 class Crud:
+    file = "data.txt"
     def __init__(self):
         print("* WELCOME TO EMP PORTAL *")
         print("===========================")
@@ -16,19 +18,63 @@ class Crud:
                 name = input("Enter your Name: ")
                 age = input("Enter your Age: ")
                 phone = input("Enter your Phone: ")
-                row = "\t".join([empid, name, age, phone, "\n"])
-                f.writelines(row)
-
-                ask = str(input("Do you want to Continue Y/N: ")).upper()
-                if ask == "Y":
-                    continue
+                if self.is_record_already_exists(empid):
+                    ask = str(input("Want to add another new record Y/N: "))
+                    if ask.upper() == "Y":
+                        print("ADDING NEW RECORD")
+                        print("===================")
+                        continue
+                    else:
+                        row = "\t".join([empid, name, age, phone, "\n"])
+                        f.writelines(row)
+                        return False
                 else:
-                    return False
+                    row = "\t".join([empid, name, age, phone, "\n"])
+                    f.writelines(row)
+
+                    ask = str(input("Want to add another new record Y/N: "))
+                    if ask.upper() == "Y":
+                        print("ADDING NEW RECORD")
+                        print("===================")
+                        continue
+                    else:
+                        return False
 
     def show_record(self):
         with open("data.txt", "r") as f:
             print(f.read())
 
+    @classmethod
+    def show_single_record(cls):
+        emp_id = input("Enter emp id to show single record: ")
+        with open(cls.file, "r") as f:
+            for line in f.readlines():
+                if emp_id in line:
+                    print("Record found")
+                    print(line)
+                    return  line
+                else:
+                    print("Record not found")
+                    break
+
+    @classmethod
+    def is_record_already_exists(cls, empid):
+        with open(cls.file, "r") as f:
+            # emp_id = input("Enter your Employee id: ")
+            # data = f.readline()
+            for line in f.readlines():
+                if empid in line:
+                    print("This record already exists please enter another record")
+                    return True
+
+    @classmethod
+    def update_record(cls):
+        emp_id = input("Enter emp id: ")
+
+
+
 obj = Crud()
 # obj.add_data()
 # obj.show_record()
+# obj.existing_record_prohibited()
+obj.show_single_record()
