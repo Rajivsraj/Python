2faee13716791ce56e36a772b317941eee6539a2
Rajiv <rajivsraj78@gmail.com>
2023-09-02 11:25:02 +0530

    numpy intro

---
+++ numpy/demo1.py
@@ -0,0 +1,30 @@
+import numpy
+# from numpy import array
+
+# arr = numpy.array([list], datatype="")
+
+# arr = numpy.array([45, 20, 67, 45, 8, 7856, 65, 12, 76], dtype="int")
+arr = numpy.array([45, 20, 67, 45, 8, 7856, 65, 12, 76])
+
+# numpy.array()
+
+for x in arr:
+    print(x)
+
+
+l = len(arr)
+# print(l)
+
+# for x in range(l):
+#     print(arr[x])
+
+#
+# i = 0;
+# while i < l:
+#     print(arr[i])
+#     i+=1
+
+# array()
+
+# print(dir(numpy))
+
