e626997a1ce8262414675a1a035fbd21d7f11376
Rajiv <rajivsraj78@gmail.com>
2023-09-16 11:13:08 +0530

    series properties:

--- all/panda/py1.py
+++ all/panda/py1.py
@@ -22,11 +22,135 @@ import numpy as np
 # print(s[0])
 
 
-n = np.array([10, 20, 30, 40])
+# n = np.array([10, 20, 30, 40])
+# s = pd.Series(n)
+# print(s)
+#
+# for x in s:
+#     print(x)
 
-s = pd.Series(n)
 
-print(s)
+# s = pd.Series([10, 20, 30, 40])
 
-for x in s:
-    print(x)
+# # Labeling / indexing
+# s = pd.Series(data=[10, 20, 30, 40], index=["hindi", "english", "skt", "maths"])
+#
+# # print series
+# print(s)
+#
+# # Access specific index value
+# print(s["hindi"])
+#
+# # Print series using loop
+# for x in s:
+#     print(x)
+
+
+# s = pd.Series(data=[10, 20, 30, 40], index=["h", "e", "m", "s"], dtype=int)
+# print(s)
+# print(type(s))
+
+# z = np.zeros(10, dtype=int)
+# print(z)
+
+
+# s = pd.Series(29, index=[0, 1, 2, 3, 4])
+# s = pd.Series(29, index=["one", "two", "three"])
+# print(s)
+
+
+
+# s = pd.Series([10, 20, 20, 40, 50, 60])
+# print(s)
+
+
+# s = pd.Series([10, 20, 30], index=["one", "two", "three"])
+# print(s.index)
+
+
+# s = pd.Series([10.0, 20.30, 20.64, 40.45, 5.50, 60.45])
+# print(s)
+# print(s.index)
+
+
+# s = pd.Series([10, 20, 30], index=[3,2,1])
+# print(s)
+# print(s.index)
+
+
+# s = pd.Series([10, 20, 30], index=[3,2,1])
+# print(s)
+# print(s.index)
+
+# Task1:  print series indexes
+
+
+# s = pd.Series([[1, 2, 3], [10, 20, 20]], index=["ind1", "ind2"])
+# print(s)
+# print(s.shape)
+
+
+# s = pd.Series([[1, 2, 3], [10, 20, 20]], index=["ind1", "ind2"])
+# s = pd.Series([10, 20, 230, 10.1, "hello"])
+# print(s)
+# print(s.dtype)
+
+
+# s = pd.Series([10, 20, 230, 10, 60, 70, "hdsdfsddfsdfe"])
+# print(s)
+# print(s.size)   # count no of elem means size of series
+
+
+# s1 = pd.Series([10, 20, 230, 10, 60, 70, "hdsdfsddfsdfe"])
+# s2 = pd.Series()
+#
+# print(s1.empty)
+# print(s2.empty)
+
+
+# s1 = pd.Series([10, 20, 230, 10, 60, np.nan, "hdsdfsddfsdfe"])
+# s2 = pd.Series()
+#
+# print(s1.hasnans)
+# print(s2.hasnans)
+
+
+
+# s1 = pd.Series([10, 20, 230, 10, 60])
+# s2 = pd.Series()
+#
+# print(s1.nbytes)
+# print(s2.nbytes)
+
+
+# s1 = pd.Series(["hi", "Hsdfki"])
+# s2 = pd.Series()
+#
+# print(s1.nbytes)
+# print(s2.nbytes)
+
+
+# s1 = pd.Series([54.45, 66.56, 67.54])
+# s2 = pd.Series()
+#
+# print(s1.nbytes)
+# print(s2.nbytes)
+
+
+# s1 = pd.Series([[1, 2, 3], [200, 30, 40]])
+# s2 = pd.Series()
+#
+# print(s1.ndim)
+# print(s2.ndim)
+
+
+# s1 = pd.Series([[1, 2, 3], [200, 30, 40]])
+# s2 = pd.Series()
+
+# print(s1.size)
+# print(s2.size)
+
+
+# dic = {"marks": 20, "name": "Rose Baby"}
+# s1 = pd.Series(dic)
+# print(s1)
