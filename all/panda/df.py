1f86da6aca4790ff7989d181e2e5ab459b649fb8
Rajiv <rajivsraj78@gmail.com>
2023-09-18 10:42:56 +0530

    Dataframe Intor

---
+++ all/panda/df.py
@@ -0,0 +1,24 @@
+# DataFrame
+
+import pandas as pd
+import numpy as np
+# Way to create df
+    # - list
+    # - dic
+    # - numpy array - task
+
+
+
+# lst = [[10, 20, 30], [10, 20, 30]]
+
+
+# dic = {
+#     "marks": [10, 20, 30],
+#     "name": ["Rahul", np.nan, "amit"]
+# }
+# s = pd.DataFrame(dic)
+# print(s)
+
+
+# Seareis
+# show sereis data b/w 3 to 7 index without loop
